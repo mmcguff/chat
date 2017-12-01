64d438412dd205ffd35faaf17d81b0422b6d5797
Matthew McGuff <matthewmcguff85@gmail.com>
2017-12-01 08:04:54 -0700

    fourth time is a charm

---
+++ public/main/main.Ctrl.js
@@ -0,0 +1,42 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('app')
+    .controller('MainCtrl', MainCtrl);
+
+  MainCtrl.$inject = ['$scope', '$localStorage', 'socket', 'lodash'];
+
+  function MainCtrl($scope, $localStorage, socket, lodash) {
+    $scope.message ='';
+    $scope.messages = [];
+    $scope.users = [];
+    $scope.mynickname = $localStorage.nickname;
+    var nickname = $scope.mynickname;
+
+    socket.emit('get-users');
+
+     socket.on('all-users', function (data) {
+      console.log(data);
+      $scope.users = data.filter(function (item) {
+        return item.nickname !== nickname;
+      });
+    });
+
+    socket.on('message-recieved', function(data){
+      $scope.messages.push(data);
+    })
+
+    $scope.sendMessage = function(date){
+      var newMessage = {
+        message: $scope.message,
+        from: $scope.mynickname
+      }
+      socket.emit('send-message', newMessage);
+      $scope.message = '';
+      $scope.messages.push(newMessage);
+    }
+
+
+  };
+})();
---
+++ server.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+var port = 3000;
+var users= [];
+
+app.use(express.static(path.join(__dirname, "public")));
+
+io.on('connection', function(socket) {
+  console.log('new connection made');
+
+  //Show all users when first logged on 
+  socket.on('get-users', function(){
+    socket.emit('all-users', users);
+  });
+
+  //When new sockets join
+  socket.on('join',function(data){
+    console.log(data); //nickname
+    console.log(users);
+    socket.nickname = data.nickname;
+    users[socket.nickname] = socket;
+    var userObj = {
+      nickname: data.nickname,
+      socketid: socket.id      
+    }
+    users.push(userObj);
+    io.emit('all-users', users);
+  });
+
+  //Broadcast the message
+  socket.on('send-message', function(data){
+    socket.broadcast.emit('message-recieved', data);
+    //io.emit('message-received',data);
+  })
+
+});
+
+server.listen(process.env.PORT || port)
+// server.listen(port, function() {
+//   console.log("Listening on port " + port);
+// });
+
+console.log(`listening on port ${port}`);
